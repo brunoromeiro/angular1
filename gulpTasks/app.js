18a34abb15d2466480b8e642e8c2f9ee8e8b4a87
brunoromeiro <bruno.romeiro@gmail.com>
2017-07-03 16:31:29 -0300

    Esqueleto Gulp
    Esqueleto Gulp

---
+++ gulpTasks/app.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp')
+
+gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets'])
+
+gulp.task('app.html', function(){ //aplicação HTML
+
+})
+
+gulp.task('app.css', function(){ //aplicação CSS
+
+})
+
+gulp.task('app.js', function(){ //caplicação  JS
+
+})
+
+gulp.task('app.assets', function(){ //Imagens
+
+})
