b1f6b1957e6d3c4240af578f24361dfb0e45c234
brunoromeiro <bruno.romeiro@gmail.com>
2017-07-07 18:34:33 -0300

    Aula 83 com erro

---
+++ app/billingCycle/billingCycleController.js
@@ -0,0 +1,22 @@
+(function() {
+  angular.module('primeiraApp').controller('BillingCycleCtrl', [
+    '$http',
+    'msgs',
+    BillingCycleController
+  ])
+
+  function BillingCycleController($http, msgs){
+
+    const vm = this
+
+    vm.create = function() {
+      const url = 'http://localhost:3003/api/billingCycles'
+      $http.post(url, vm.billingCycle).then(function(response) {
+        vm.billingCycle = {}
+        msgs.addSuccess('Operação realizada com Sucesso!')//console.log('Sucesso!')
+      }).error(function(data) {
+        msgs.addError(response.data.errors)
+      })
+    }
+  }
+})()
