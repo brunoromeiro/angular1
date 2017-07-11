08b957c324853993e7858f6d88d401a104fd4e62
brunoromeiro <bruno.romeiro@gmail.com>
2017-07-11 15:47:33 -0300

    5

--- app/common/factories/msgsFactory.js
+++ app/common/factories/msgsFactory.js
@@ -1,5 +1,32 @@
-(function() {
-  angular.module('primeiraApp').factory('msgs', [
+/*Versão do Professor*/
+angular.module('primeiraApp').factory('msgs', [
+  'toastr',
+  MsgsFactory
+])
+
+function MsgsFactory(toastr) {
+  function addSuccess(msgs) {
+    addMsg(msgs, 'Sucesso', 'success')
+  }
+
+  function addError(msgs) {
+    addMsg(msgs, 'Erro', 'error')
+  }
+
+  function addMsg(msgs, title, method) {
+    if(msgs instanceof Array) {
+      msgs.forEach(msg => toastr[method](msg, title))
+    } else {
+      toastr[method](msgs, title)
+    }
+  }
+
+  return { addSuccess, addError }
+}
+
+/* MINHA VERSÂO DO CODIGO
+
+angular.module('primeiraApp').factory('msgs', [
   'toastr',
   MsgsFactory
 ])
@@ -24,4 +51,4 @@ function addSuccess(msgs) {
 
   return { addSuccess, addError }
 }
-})()
+*/
